71e661b43d32512ae26e4b6207c65aee95d2452e
rudy870103 <favoriteinfinite@gmail.com>
2023-12-11 10:47:01 +0800

    完成最新消息後台功能

--- back/news.php
+++ back/news.php
@@ -1,13 +1,12 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">動畫圖片管理</p>
+    <p class="t cent botli">最新消息資料管理</p>
     <form method="post" action="./api/edit.php">
         <table width="100%" style="text-align: center">
             <tbody>
                 <tr class="yel">
-                    <td width="70%">動畫圖片</td>
+                    <td width="80%">最新消息資料內容</td>
                     <td width="10%">顯示</td>
                     <td width="10%">刪除</td>
-                    <td></td>
                 </tr>
                 <?php
                 $rows=$DB->all();
@@ -15,17 +14,13 @@
                 ?>
                 <tr>
                     <td>
-                        <img src="./img/<?=$row['img'];?>" style="width:150px;height:100px">
+                        <textarea name="text[<?=$row['id'];?>]" style="width:90%;height:60px"><?=$row['text'];?></textarea>
                     </td>
-                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     <td>
-                    <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
+                        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
                     </td>
                     <td>
                     <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                    </td>
-                    <td>
-                    <input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更換動畫">
                     </td>
                 </tr>
                 <?php
@@ -36,8 +31,8 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <input type="hidden" name="table" value="<?= $do; ?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增動畫圖片"></td>
+                    <input type="hidden" name="table" value="<?=$do;?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增最新消息資料"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
