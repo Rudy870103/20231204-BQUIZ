43996c09fa0cf1de7f79821c394272b84af51353
rudy870103 <favoriteinfinite@gmail.com>
2023-12-15 13:13:21 +0800

    新增marquee檔案，include進前端網頁

---
+++ front/marquee.php
@@ -0,0 +1,9 @@
+<marquee behavior="" direction="">
+    <?php
+    $ads = $Ad->all(['sh' => 1]);
+    foreach ($ads as $ad) {
+        echo $ad['text'];
+        echo '&nbsp&nbsp/&nbsp&nbsp';
+    }
+    ?>
+</marquee>
