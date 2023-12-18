4170187601698ba56255fe03e59e098474f075e6
rudy870103 <favoriteinfinite@gmail.com>
2023-12-18 13:59:47 +0800

    完成登入登出，帳號密碼檢查功能

--- index.php
+++ index.php
@@ -82,7 +82,7 @@
 				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
 				<div style="width:89%; height:480px;" class="dbor">
 					<span class="t botli">校園映象區</span>
-					<div class="cent" onclick="pp(1)"><img src="./icon/01E01.jpg" alt=""></div>
+					<div class="cent" onclick="pp(1)"><img src="./icon/up.jpg" alt=""></div>
 					<?php
 					$imgs = $Image->all(['sh' => 1]);
 
@@ -94,7 +94,7 @@
 					<?php
 					}
 					?>
-					<div class="cent" onclick="pp(2)"><img src="./icon/01E02.jpg alt=""></div>
+					<div class="cent" onclick="pp(2)"><img src="./icon/dn.jpg" alt=""></div>
 					<script>
 						var nowpage = 1,
 							num = <?= $Image->count(['sh' => 1]); ?>;
